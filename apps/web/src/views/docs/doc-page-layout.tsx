a0e7bb00b60e58ad587d2d3ad31fadcd9a877a9e
Prashanth Kumar <prash8669@yahoo.com>
2026-06-21 10:24:19 +0530

    refactor: remove legacy documentation view components and related routing files

--- apps/web/src/views/docs/doc-page-layout.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import type React from "react";
-
-export function DocPageLayout({ children }: { children: React.ReactNode }) {
-  return <div data-doc-content="">{children}</div>;
-}
